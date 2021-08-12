be66f2cab84951e4b8562f3dfafed2396a0a0a00
ignacioM3 <85070038+ignacioM3@users.noreply.github.com>
2021-08-12 00:21:53 -0300

    Arreglos en user/producDetail

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,8 @@ let controllers = require('../controllers/productsController')
 
 router.get('/list', controllers.list);
 router.get('/detail/:id', controllers.detail);
-router.get('/cart', controllers.cart);
-router.get('/create', controllers.create);
+router.get('/', controllers.cart);
+router.get('/cresate', controllers.create);
 router.get('/edit/:id', controllers.edit);
 
 module.exports = router;
