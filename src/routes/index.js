1da3852539ea1665c9e626eeb802ca8dffe32a5b
EALucero <ea.lucero@outlook.com>
2021-08-19 20:29:24 -0300

    multer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let { home, contact } = require('../controllers/indexController')
+let { home, contact } = require('../controllers/indexController');
 
 router.get('/', home);
 router.get('/contact', contact);
---
+++ src/routes/multer.js
@@ -0,0 +1,12 @@
+const multer = require('multer');
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+        cb(null, './public/images/avatars'); 
+     }, 
+     filename: function (req, file, cb) { 
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  
+     } 
+   });
+const uploadFile = multer({ storage });
+
+module.export = uploadFile;
