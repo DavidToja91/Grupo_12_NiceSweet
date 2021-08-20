fff6647dcf792f5a022526a8b478f840b97150e8
EALucero <ea.lucero@outlook.com>
2021-08-20 00:41:01 -0300

    mini arreglos

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,7 +7,7 @@ let {inicio,
     editarFormulario,
     editarProducto, 
     eliminarProducto} = require('../controllers/adminController.js');
-// let uploadFile = require('../middlewares/uploadFiles');
+let uploadFile = require('../middlewares/uploadFiles');
 
 /* GET: Index para el admin */
 router.get('/' , inicio);
@@ -23,7 +23,7 @@ router.get('/products' , productos);
 /* GET: Formulario para agregar productos.*/
 router.get('/agregarProducto/' , agregarFormulario); /*Agrega un producto nuevo al formulario */
 /*POST : Formulario para capturar los datos recibidos */
-router.post('/agregarProducto' , agregarProducto); /*Envía los datos del formulario */
+router.post('/agregarProducto' , uploadFiles.single('image'), agregarProducto); /*Envía los datos del formulario */
 // uploadFile.single('image')
 
 /* GET: formulario de edición de productos */
