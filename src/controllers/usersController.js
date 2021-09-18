1265be921ecf13a12a398a9900c27c9628723925
DavidToja91 <hernan.rocha.toja@hotmail.com>
2021-09-18 11:13:37 -0300

    redirect /login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -47,7 +47,7 @@ module.exports = {
 
             writeUsersJSON(getUsers);
 
-            res.redirect('/')
+            res.redirect('/users/login')
         } else{
             res.render('users/register',{
                 errors : errors.mapped(),
