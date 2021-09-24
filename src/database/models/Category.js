ed9a917d09fc4d0bfaffa07a79334a886d25e413
Fer Valdez <valdezfernanda17@gmail.com>
2021-09-23 22:13:12 -0300

    modelos, faltan terminar

---
+++ src/database/models/Category.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Categories";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(11).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false 
+        },
+        nameCategory:{
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+    }
+    let config = {
+        tableName: "categories",
+        timeStamps: false
+    }
+
+    const Category = sequelize.define(alias, cols, config)
+//se enlaza con subcategorias
+    Category.associate = models => {
+        Category.hasMany(models.Subcategories , {
+            as: "subcategories",
+            foreignKey: "categoryId"
+        })
+
+    }
+
+    return Category;
+}
---
+++ src/database/models/Product.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Products";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(11).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false 
+        },
+        nameProduct: {
+            type: dataTypes.STRING(20),
+            allowNull: false
+        },
+        price: {
+            type: dataTypes.INTEGER(11),
+            allowNull: false
+        },
+        discount: {
+            type: dataTypes.INTEGER(11),
+        },
+        description: {
+            type: dataTypes.STRING(200),
+        },
+        subcategoryId: {
+            type: dataTypes.INTEGER(11),
+            allowNull: false
+        },
+        image: {
+            type: dataTypes.TEXT(50),
+        },
+    }
+    let config = {
+        tableName: "products",
+        timeStamps: true
+    }
+
+    const Product = sequelize.define(alias, cols, config)
+
+    Product.associate = models =>{
+        Product.belongsTo(models.Subcategories,{
+            as: "subcategory",
+            foreignKey: "subcategoryId"
+        })
+    }
+
+    return Product;
+}
