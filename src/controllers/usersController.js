51137161368739064c6e9543994cf31239a1c210
EALucero <ea.lucero@outlook.com>
2021-10-01 00:57:44 -0300

    userController

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,32 +1,10 @@
+const{validationResult}= require('express-validator');
+const bcrypt= require('bcryptjs');
 const db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
 const User = db.Users;
-
-
-module.exports = {
-    index: (req, res) => {
-        Product.findAll()
-            .then(products =>{
-                res.render('products/list.ejs', {products})
-            })
-    },
-    detail: (req, res) => {
-        Product.findByPk(req.params.id)
-        .then(product =>{
-            res.render('products/detail.ejs', {product})
-        })
-    },
-    cart: (req, res)=>{
-
-    },
-
-};
-
-let {getUsers, writeUsersJSON } = require('../data/usersDB');
-const{validationResult}= require('express-validator');
-const bcrypt= require('bcryptjs');
 
 module.exports = {
     register: (req, res) => {
@@ -37,124 +15,121 @@ module.exports = {
     }, 
     processRegister: (req, res) => {     
         let errors = validationResult(req);
-        
+
+        if (req.fileValidatorError) {
+            let image = {
+                param: "image",
+                msg: req.fileValidatorError,
+            };
+
+            errors.push(image);
+        }
+
         if (errors.isEmpty()) {
+            let { name, lastName, phone, email, pass1 } = req.body;
 
-            const { name, lastName, email, password, phoneNumber, rol, avatar } = req.body;
+            User.create({
+                name,
+                lastName,
+                phoneNumber: phone,
+                email,
+                password: bcrypt.hashSync(pass1, 10),
+                avatar: req.file? req.file.filename : "default-image.png",
+                rol: 0,
+            }).then(() => res.redirect("users/login"));
 
-            User.findAll({ include: [{ association: 'userProducts' }] })
-                .then(users => res.status(200).json(users));
-
-            if (User.email != email) {
-                password = bcrypt.hashSync(pass1, 10);
-                rol = 'USER';
-                avatar = req.file? req.file.filename : "default.png";
-                
-                User.create( name, lastName, email, password, phoneNumber, rol, avatar )
-                .then(user => res.status(201).json(user))
-                .catch(error => console.log(error)); 
-
-                res.redirect('/users/login');
-            }
-
-        } else{
-            res.render('users/register',{
-                errors : errors.mapped(),
-                old: req.body
+        } else {
+            res.render("users/register", {
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session,
             });
         }
     },
     login: (req, res) => {        
         res.render('users/login', {
-            title: "¡Inicia sesión!",
             session: req.session
         });
     },
     processLogin: (req, res) => {
         let errors = validationResult(req);
-            
-        if(errors.isEmpty()){
 
-            let user = getUsers.find(user => user.email === req.body.email);
+        if (errors.isEmpty()) {
+            User.findOne({
+                where: { email: req.body.email },
+            }).then(user => {
+                req.session.user = {
+                    id: user.id,
+                    name: user.name,
+                    last_name: user.last_name,
+                    phone: user.phone,
+                    email: user.email,
+                    avatar: user.avatar,
+                    rol: user.rol,
+                };
 
-            req.session.user = { 
-                id: user.id,
-                name: user.name,
-                last_name: user.last_name,
-                email: user.email,
-                avatar: user.avatar,
-                category: user.category,
-                phone: user.phone
-            }
+                if (req.body.remember) {
+                    res.cookie("niceSweet", req.session.user, {
+                        expires: new Date(Date.now() + 900000),
+                        httpOnly: true,
+                        secure: true,
+                    });
+                }
 
-            if(req.body.remember) {
-                res.cookie('niceSweet',req.session.user,{expires: new Date(Date.now() + 900000), httpOnly: true})
-            }
-
-            res.locals.user = req.session.user;
-        
-            res.redirect('/')
-
-        } else{
-            res.render('users/login', {
-                errors: errors.mapped(), 
-                session: req.session 
+                res.locals.user = req.session.user;           
+                res.redirect("/");
+            });
+        } else {
+            res.render("users/login", {
+                errors: errors.mapped(),
+                session: req.session,
             });
         }
     },
     profile: (req, res) => {     
-        let user = getUsers.find(user=> user.id === req.session.user.id);
-
-        res.render('users/profile',{
-            title: "¡Tus datos!",
-            session: req.session,
-            user
+        User.findByPk(req.session.user.id)
+        .then(user => {
+              res.render("users/profile", {
+                user,
+                session: req.session
+              });
         });
     },
     editProfile: (req, res) => {
-        let user = getUsers.find(user => user.id === +req.params.id)
-
-        res.render('users/edit', {
-            title: "¡Tus datos!",
-            user,
-            session: req.session
-        })
+        User.findByPk(req.params.id)
+        .then(user => {
+            res.render("users/edit", {
+                user,
+                session: req.session,
+            });
+        });
     },
-    updateProfile: (req, res) => {        
+    updateProfile: (req, res) => {
         let errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+
+            let { name, lastName, phone } = req.body;
             
-        if(errors.isEmpty()){
-            let user = getUsers.find(user => user.id === +req.params.id)
-            
-            let { 
-                name, 
+            User.update({
+                name,
                 lastName,
-                phone,
-            } = req.body;
-
-            user.id = user.id
-            user.name = name
-            user.lastName = lastName
-            user.phone = phone
-            user.avatar = req.file ? req.file.filename : user.avatar
-
-            writeUsersJSON(getUsers);
-
-            delete user.pass;          
-            req.session.user = user;
-            
-            res.redirect("/users/profile");
+                phoneNumber: phone,
+                avatar: req.file && req.file.filename,
+            }, { where: { id: req.params.id } })
+            .then(() => res.redirect("/users/profile"));
 
         } else {
-            res.render('users/edit', {
+            res.render("users/edit", {
                 errors: errors.mapped(),
                 old: req.body,
-                session: req.session 
-            }); 
+                session: req.session,
+            });
         }
     },
     logout: (req, res) =>{
         req.session.destroy();
+        
         if(req.cookies.userNiceSweet){
             res.cookie('userNiceSweet','',{maxAge: -1})
         }
