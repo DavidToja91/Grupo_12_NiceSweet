d4451b26916f0905a80016fba8f149c6a1a177cb
ignacioM3 <85070038+ignacioM3@users.noreply.github.com>
2021-10-23 16:37:49 -0300

    Termina validaciones de register

--- public/Js/validatorRegister.js
+++ public/Js/validatorRegister.js
@@ -8,11 +8,22 @@ window.addEventListener('load',function(){
     $nameErrors = qs('#nameErrors'),
     $inputLastname = qs('#lastname'),
     $lastnameErrors = qs('#lastnameErrors'),
+    $email = qs('#email'),
+    $emailErrors = qs('#emailErrors'),
+    $pass = qs('#pass'),
+    $passErrors = qs('#passErrors'),
+    $pass2 = qs('#pass2'),
+    $pass2Errors = qs('#pass2Errors'),
+    $terms = qs('#check'),
+    $termsErrors = qs('#termsErrors'),
+    $file = qs('#formFile'),
+    $fileErrors = qs('#fileErrors'),
     regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
     regExDNI = /^[0-9]{7,8}$/,
     regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
     regExPass = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,12}$/;
 
+    //Nombre
     $inputName.addEventListener('blur', function(){
         console.log($inputName.value.trim())
         switch (true) {
@@ -36,7 +47,7 @@ window.addEventListener('load',function(){
         }
     })
 
-
+    //Apellido
     $inputLastname.addEventListener('blur', function(){
         console.log($inputLastname.value.trim())
         switch (true) {
@@ -48,8 +59,8 @@ window.addEventListener('load',function(){
                 $lastnameErrors.innerHTML = 'Ingresa un Apellido valido'
                 $inputLastname.classList.add('is-invalid')
                 break;
-            case $inputLastname.value.trim().length <= 2:
-                    $lastnameErrors.innerHTML = 'El Apellido debe tener más de 2 caracteres'
+            case $inputLastname.value.trim().length <= 1:
+                    $lastnameErrors.innerHTML = 'El Apellido debe tener más de un caracter'
                     $inputLastname.classList.add('is-invalid')
                 break;
             default:
@@ -59,4 +70,97 @@ window.addEventListener('load',function(){
             break;
         }
     })
+    //Email
+    $email.addEventListener('blur', function(){
+        switch (true) {
+            case !$email.value.trim():
+                $emailErrors.innerHTML = 'El campo email es obligatorio'
+                $email.classList.add('is-invalid')
+                break;
+            case !regExEmail.test($email.value):
+                $emailErrors.innerHTML = 'Debe ingresar un email válido'
+                $email.classList.add('is-invalid')
+                break;    
+            default:
+                $email.classList.remove("is-invalid");
+                $email.classList.add('is-valid')
+                $emailErrors.innerHTML = ""
+                break;
+        }
+    })
+
+    //password
+    $pass.addEventListener('blur', function(){
+        switch (true) {
+            case !$pass.value.trim():
+                $passErrors.innerHTML = 'El campo contraseña es obligatorio'
+                $pass.classList.add('is-invalid')
+                break;
+            case !regExPass.test($pass.value):
+                $passErrors.innerHTML = 'La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
+                $pass.classList.add('is-invalid')
+                break;    
+            default:
+                $pass.classList.remove("is-invalid");
+                $pass.classList.add('is-valid')
+                $passErrors.innerHTML = ""
+                break;
+        }
+    })
+
+    //password 2
+    $pass2.addEventListener('blur', function(){
+        switch (true) {
+            case !$pass2.value.trim():
+                $pass2Errors.innerHTML = 'Reingresa la contraseña'
+                $pass2.classList.add('is-invalid')           
+            break;
+
+            
+            case $pass2.value !== $pass.value:
+                $pass2Errors.innerHTML = 'las contraseñas no coinciden'
+                $pass2.classList.add('is-invalid')           
+            break;
+                
+            default:
+                $pass2.classList.remove('is-invalid')
+                $pass2.classList.add('is-valid')
+                $pass2Errors.innerHTML = ""
+            break;
+        }
+    })
+
+    //Terminos y Condiciones
+    $terms.addEventListener('click',()=>{
+        $terms.value = "on"
+        $terms.classList.toggle('is-valid')
+        $terms.classList.remove('is-invalid')
+        $termsErrors.innerHTML = ""
+    })
+
+    //Img
+    $file.addEventListener('change',
+    function fileValidation() {
+        let filePath = $file.value, //Capturo el valor del input
+            allowefExtensions = /(.jpg|.jpeg|.png|.gif|.web)$/i //Extensiones permitidas
+        if (!allowefExtensions.exec(filePath)) { //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
+            $fileErrors.innerHTML = 'Carga un archivo de imagen válido, con las extensiones (.jpg - .jpeg - .png - .gif)';
+            $file.value = '';
+            $imgPreview.innerHTML = '';
+            return false;
+        } else {
+            // Image preview
+            console.log($file.files);
+            if ($file.files && $file.files[0]) {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    $imgPreview.innerHTML = '<img src="' + e.target.result + '"/>';
+                };
+                reader.readAsDataURL($file.files[0]);
+                $fileErrors.innerHTML = '';
+                $file.classList.remove('is-invalid')
+            }
+        }
+    })
+
 })
