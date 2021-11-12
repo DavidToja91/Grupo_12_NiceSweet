989e1c9c156161e9c14ab9774f219e90b848be84
EALucero <ea.lucero@outlook.com>
2021-11-12 11:07:47 -0300

    validators

--- public/Js/validatorEditUser.js
+++ public/Js/validatorEditUser.js
@@ -1,5 +1,5 @@
 function qs(element){
-    return document.querySelector(element)
+    return document.querySelector(element);
 }
 
 window.addEventListener('load', function(){
@@ -10,78 +10,95 @@ window.addEventListener('load', function(){
     $phoneNumber =qs('#phoneNumber'),
     $phoneNumberErrors =qs('#phoneNumberErrors'),
     $btn =qs('#btn'),
-    regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/
+    regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/;
 
-      //Name
-      $name.addEventListener('blur', function(){
-        console.log($name.value.trim())
+    //Name
+    $name.addEventListener('blur', function(){
+        console.log($name.value.trim());
         switch (true) {
             case !$name.value.trim():
-                $nameErrors.innerHTML = 'El campo nombre es obligatorio'
-                $name.classList.add('is-invalid')
+                $nameErrors.innerHTML = 'El campo nombre es obligatorio';
+                $name.classList.add('is-invalid');
             break;
             case !regExAlpha.test($name.value):
-                $nameErrors.innerHTML = 'Ingresa un nombre valido'
-                $name.classList.add('is-invalid')
+                $nameErrors.innerHTML = 'Ingresa un nombre valido';
+                $name.classList.add('is-invalid');
                 break;
             case $name.value.trim().length <= 2:
-                    $nameErrors.innerHTML = 'El nombre debe tener más de 2 caracteres'
-                    $name.classList.add('is-invalid')
+                    $nameErrors.innerHTML = 'El nombre debe tener más de 2 caracteres';
+                    $name.classList.add('is-invalid');
                 break;
             default:
-                $name.classList.remove('is-invalid')
-                $name.classList.add('is-valid')
-                $nameErrors.innerHTML = ""
+                $name.classList.remove('is-invalid');
+                $name.classList.add('is-valid');
+                $nameErrors.innerHTML = "";
             break;
         }
-} )
+    });
+
     //Last name
     $lastName.addEventListener('blur', function(){
-        console.log($lastName.value.trim())
+        console.log($lastName.value.trim());
         switch (true) {
             case !$lastName.value.trim():
-                $lastnameErrors.innerHTML = 'El campo Apellido es obligatorio'
-                $lastName.classList.add('is-invalid')
+                $lastnameErrors.innerHTML = 'El campo Apellido es obligatorio';
+                $lastName.classList.add('is-invalid');
             break;
             case !regExAlpha.test($inputLastname.value):
-                $lastnameErrors.innerHTML = 'Ingresa un Apellido valido'
-                $lastName.classList.add('is-invalid')
+                $lastnameErrors.innerHTML = 'Ingresa un Apellido valido';
+                $lastName.classList.add('is-invalid');
                 break;
             case $lastName.value.trim().length <= 1:
                     $lastnameErrors.innerHTML = 'El Apellido debe tener más de un caracter'
-                    $lastName.classList.add('is-invalid')
+                    $lastName.classList.add('is-invalid');
                 break;
             default:
-                $lastName.classList.remove('is-invalid')
-                $lastName.classList.add('is-valid')
-                $lastnameErrors.innerHTML = ""
+                $lastName.classList.remove('is-invalid');
+                $lastName.classList.add('is-valid');
+                $lastnameErrors.innerHTML = "";
             break;
         }
     })
 
-        //PhoneNumber
-        $phoneNumber.addEventListener('blur', function(){
-            console.log($phoneNumber.value.trim())
-            switch (true) {
-                case !$phoneNumber.value.trim():
-                    $phoneNumber.innerHTML = 'El campo Teléfono es requerido.'
-                    $phoneNumber.classList.add('is-invalid')
-                break;
-                case $phoneNumber.value.trim().length <= 10:
-                        $phoneNumberErrors.innerHTML = 'El número ingresado no contiene los caracteres requeridos'
-                        $phoneNumber.classList.add('is-invalid')
-                    break;
-                default:
-                    $phoneNumber.classList.remove('is-invalid')
-                    $phoneNumber.classList.add('is-valid')
-                    $phoneNumberErrors.innerHTML = ""
-                break;
+    //PhoneNumber
+    $phoneNumber.addEventListener('blur', function(){
+        console.log($phoneNumber.value.trim());
+        switch (true) {
+            case !$phoneNumber.value.trim():
+                $phoneNumber.innerHTML = 'El campo Teléfono es requerido.';
+                $phoneNumber.classList.add('is-invalid');
+            break;
+            case $phoneNumber.value.trim().length <= 10:
+                $phoneNumberErrors.innerHTML = 'El número ingresado no contiene los caracteres requeridos';
+                $phoneNumber.classList.add('is-invalid');
+            break;
+            default:
+                $phoneNumber.classList.remove('is-invalid');
+                $phoneNumber.classList.add('is-valid');
+                $phoneNumberErrors.innerHTML = "";
+            break;
+        }
+    });
+
+    $btn.addEventListener("submit", function (e) {
+        e.preventDefault();
+        let error = false;
+        let formElements = this.elements;
+        console.log(formElements);
+        for (let index = 0; index < formElements.length - 1; index++) {
+            if (
+                formElements[index].value === "" ||
+                formElements[index].classList.contains('is-invalid')
+            ) {
+                formElements[index].classList.add('is-invalid');
+                submitErrors.innerHTML = "Los campos señalados son obligatorios";
+                error = true;
             }
-        })
-
-        $btn.addEventListener('submit', function(e){
-            e.preventDefault()
-        })
-
-
-})
+        }
+    
+        if (!error) {
+            console.log('Todo bien');
+            $form.submit();
+        }
+    });
+});
