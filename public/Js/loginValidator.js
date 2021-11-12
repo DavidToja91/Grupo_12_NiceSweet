6551415c338987b321503fc862bdd5558b6cdeda
EALucero <ea.lucero@outlook.com>
2021-11-12 10:27:05 -0300

    prolijidad

--- public/Js/loginValidator.js
+++ public/Js/loginValidator.js
@@ -1,66 +1,66 @@
 function qs(element) {
-    return document.querySelector(element)
+    return document.querySelector(element);
 }
 
 window.addEventListener('load', function () {
-    let $form = qs('#form')
-    let $email = qs('#email')
-    let $emailErrors = qs('#emailErrors')
-    let $password = qs('#password')
-    let $passErrors = qs('#passErrors')
-    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
-    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
-    let $terms = qs('#check')
-    let $termsErrors = qs('#termsErrors')
+    let $form = qs('#form'),
+    $email = qs('#email'),
+    $emailErrors = qs('#emailErrors'),
+    $password = qs('#password'),
+    $passErrors = qs('#passErrors'),
+    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/,
+    $terms = qs('#check'),
+    $termsErrors = qs('#termsErrors');
 
     $email.addEventListener('blur', function () {
         switch (true) {
             case !$email.value.trim():
-                $emailErrors.innerHTML = 'El campo email es obligatorio'
-                $email.classList.add('is-invalid')
+                $emailErrors.innerHTML = 'El campo email es obligatorio';
+                $email.classList.add('is-invalid');
                 break;
             case !regExEmail.test($email.value):
-                $emailErrors.innerHTML = 'Debe ingresar un email válido'
-                $email.classList.add('is-invalid')
+                $emailErrors.innerHTML = 'Debe ingresar un email válido';
+                $email.classList.add('is-invalid');
                 break;
             default:
                 $email.classList.remove("is-invalid");
-                $email.classList.add('is-valid')
-                $emailErrors.innerHTML = ""
+                $email.classList.add('is-valid');
+                $emailErrors.innerHTML = "";
                 break;
         }
-    })
+    });
 
     $password.addEventListener('blur', function () {
         switch (true) {
             case !$password.value.trim():
-                $passErrors.innerHTML = 'El campo contraseña es obligatorio'
-                $pass.classList.add('is-invalid')
+                $passErrors.innerHTML = 'El campo contraseña es obligatorio';
+                $pass.classList.add('is-invalid');
                 break;
             case !regExPass.test($password.value):
                 $passErrors.innerHTML = 'La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
-                $password.classList.add('is-invalid')
+                $password.classList.add('is-invalid');
                 break;
             default:
                 $password.classList.remove("is-invalid");
-                $password.classList.add('is-valid')
-                $passErrors.innerHTML = ""
+                $password.classList.add('is-valid');
+                $passErrors.innerHTML = "";
                 break;
         }
 
         $terms.addEventListener('click', function () {
-            $terms.value = 'on'
-            $terms.classList.toggle('is-valid')
-            $terms.classList.remove('is-invalid')
-            $termsErrors.innerHTML = ''
+            $terms.value = 'on';
+            $terms.classList.toggle('is-valid');
+            $terms.classList.remove('is-invalid');
+            $termsErrors.innerHTML = '';
         })
-    })
+    });
 
     $form.addEventListener("submit", function (e) {
         e.preventDefault();
         let error = false;
         let formElements = this.elements;
-        console.log(formElements)
+        console.log(formElements);
         for (let index = 0; index < formElements.length - 1; index++) {
             if (
                 formElements[index].value === "" ||
@@ -74,7 +74,7 @@ window.addEventListener('load', function () {
 
         if (!error) {
             console.log('Todo bien');
-            $form.submit()
+            $form.submit();
         }
     });
-})
+});
